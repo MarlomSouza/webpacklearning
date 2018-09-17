0a958d41e7bd89cec7ebdf642688e058390bdcbf
Marlom Souza <marlom1012@gmail.com>
2018-09-16 22:01:43 -0400

    style

--- src/index.jsx
+++ src/index.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from "react-dom";
 import { HelloWorld } from './components/HelloWorld';
 
-// import './styles/app.scss';
+import './styles/app.scss';
 
 ReactDOM.render(
     <HelloWorld />,
